8f783fde6ae603b1c880c2f8b7eab07079573160
Nikolai Ponomarev <wowasterdev@gmail.com>
2022-10-01 14:41:58 +0300

    Add documentation comments

--- Homework1Task1/Task1/ListInsertionSortExtension.cs
+++ Homework1Task1/Task1/ListInsertionSortExtension.cs
@@ -2,6 +2,9 @@ namespace Task1;
 
 public static class ListInsertionSortExtension
 {
+    /// <summary>
+    ///     Sorts list in-place using insertion sort(O(n^2) complexity) using provided comparer.
+    /// </summary>
     public static void InsertionSort<T>(this List<T> list, IComparer<T> comparer)
     {
         var i = 1;
@@ -20,6 +23,9 @@ public static class ListInsertionSortExtension
         }
     }
 
+    /// <summary>
+    ///     Sorts list in-place using insertion sort(O(n^2) complexity) using default comparer.
+    /// </summary>
     public static void InsertionSort<T>(this List<T> list)
     {
         InsertionSort(list, Comparer<T>.Default);
